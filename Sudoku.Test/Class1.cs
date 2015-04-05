cddd4ac44064acbe14da74d2e95bbecd24e099e2
tristanles <tristanlescut@gmail.com>
2015-04-05 19:15:50 -0400

    Add unit test project and move sln file

---
+++ Sudoku.Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Sudoku.Test
+{
+    public class Class1
+    {
+
+        [Fact]
+        public void ShouldSucceed()
+        {
+            Assert.True(true);
+        }
+    }
+}
